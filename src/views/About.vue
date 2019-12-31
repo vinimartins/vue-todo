da41ae8d0325cf05dfcdca06281e5a22ab84c58c
Vinicius Manara <vinimanara@hotmail.com>
2019-12-31 16:52:14 -0300

    First Commit

---
+++ src/views/About.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="about">
+    <h1>Sobre:</h1>
+    <p>Exemplo de crud utilizando VUE</p>
+    
+    <p><a href="https://jsonplaceholder.typicode.com/todos">API utilizada</a> somente para exemplificar </p>
+  </div>
+</template>
